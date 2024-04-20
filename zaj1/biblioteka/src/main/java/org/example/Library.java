129448d383a1886d392e24fe062a9a28499780fa
Michał Ślifierski <m.slifierski@ufin.pl>
2024-04-20 15:28:50 +0200

    update lib / add giveBack() method

--- zaj1/biblioteka/src/main/java/org/example/Library.java
+++ zaj1/biblioteka/src/main/java/org/example/Library.java
@@ -6,23 +6,34 @@ import java.util.List;
 
 public class Library {
     private final List<Book> books = new ArrayList<Book>();
+    private final List<Book> borrowed = new ArrayList<Book>();
 
     public void init(Book[] books) {
         this.books.addAll(Arrays.asList(books));
     }
 
     public Book borrow(String author, String title) {
-        int i = searchForBook(author, title);
+        int i = searchForBook(this.books, author, title);
         if (i<0) return null;
         Book b = this.books.get(i);
         this.books.remove(i);
+        this.borrowed.add(b);
         return b;
     }
 
-    private int searchForBook(String author, String title) {
-        for (int i=0; i<this.books.size(); ++i) {
-            Book b = this.books.get(i);
-            if (b.getAuthor() == author && b.getTitle() == b.getTitle()) {
+    public boolean giveBack(Book book) {
+        int i = searchForBook(this.borrowed, book.getAuthor(), book.getTitle());
+        if (i<0) return false;
+        Book b = this.borrowed.get(i);
+        this.borrowed.remove(i);
+        this.books.add(b);
+        return true;
+    }
+
+    private int searchForBook(List<Book> books, String author, String title) {
+        for (int i=0; i<books.size(); ++i) {
+            Book b = books.get(i);
+            if (b.getAuthor() == author && b.getTitle() == title) {
                 return i;
             }
         }
--- zaj1/biblioteka/src/test/java/org/example/LibraryTest.java
+++ zaj1/biblioteka/src/test/java/org/example/LibraryTest.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.List;
@@ -57,11 +59,16 @@ class LibraryTest {
         this.lib.init(books);
 
         List<Book> listBooks;
+        List<Book> borrowed;
         try {
-            Field f = lib.getClass().getDeclaredField("books");
-            f.setAccessible(true);
-            listBooks = (List<Book>) f.get(lib);
-            f.setAccessible(false);
+            Field f1 = lib.getClass().getDeclaredField("books");
+            f1.setAccessible(true);
+            listBooks = (List<Book>) f1.get(lib);
+            f1.setAccessible(false);
+            Field f2 = lib.getClass().getDeclaredField("borrowed");
+            f2.setAccessible(true);
+            borrowed = (List<Book>) f2.get(lib);
+            f2.setAccessible(false);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -73,6 +80,43 @@ class LibraryTest {
         // *** THEN ***
         assertNull(notFound, "This book should not exists.");
         assertEquals(b.getId(), 1);
+        assertEquals(1, borrowed.size());
         assertEquals(2, listBooks.size());
+    }
+
+    @Test
+    void giveBack() {
+        // *** GIVEN ***
+        // class has books
+        Book[] books = new Book[] {
+                new Book("J.R.R. Tolkien", "Silmallirion"),
+                new Book("James Joice", "Ulisses"),
+                new Book("Mark Twain", "The adventures of Tom Sawyer")
+        };
+        this.lib.init(books);
+
+        List<Book> listBooks;
+        List<Book> borrowed;
+        try {
+            Field f1 = lib.getClass().getDeclaredField("books");
+            f1.setAccessible(true);
+            listBooks = (List<Book>) f1.get(lib);
+            f1.setAccessible(false);
+            Field f2 = lib.getClass().getDeclaredField("borrowed");
+            f2.setAccessible(true);
+            borrowed = (List<Book>) f2.get(lib);
+            f1.setAccessible(false);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        Book b = this.lib.borrow("J.R.R. Tolkien", "Silmallirion");
+
+        // *** WHEN ***
+        boolean returned = this.lib.giveBack(b);
+
+        // *** THEN ***
+        assertEquals(0, borrowed.size());
+        assertEquals(3, listBooks.size());
+        assertTrue(returned, "Book has been returned");
     }
 }
